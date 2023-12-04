7840dd882da2bfb0d74ea3c5eea460edb5df8e00
ImMohammadHosseini <sayedmh50@gmail.com>
2023-12-04 16:44:21 +0330

    fix bugs

--- dp_learning/full_finetune.py
+++ dp_learning/full_finetune.py
@@ -1,12 +1,14 @@
 
 import numpy as np
+import torch
 from torch.nn import CrossEntropyLoss
 from torch.optim import AdamW
 from opacus import PrivacyEngine
 from opacus.utils.batch_memory_manager import BatchMemoryManager
-import tqdm
+from tqdm.notebook import tqdm
 
-class Soft_Prompt():
+
+class Full_Fintune():
     def __init__(self, llm, epsilon, delta, sampling_rate, training_iteration, 
                  max_gradient_norm, noise_scale, sequence_length, embedding_dim, 
                  lr, device
@@ -25,9 +27,10 @@ class Soft_Prompt():
         self.device = device
         
         self.loss_fn = CrossEntropyLoss()
-        self.freeze_model_layers()
         
         self.total_parameter = 0
+        self.freeze_model_layers()
+
         self.optimizer = AdamW(self.llm.parameters(), lr=5e-4)
         self.privacy_engine = PrivacyEngine()
         
@@ -37,8 +40,9 @@ class Soft_Prompt():
             self.total_parameter += p.numel()
             
     def train (self, epochs, train_dataloader):
+        self.llm.train()
         llm_model, optimizer, train_dataloader = self.privacy_engine.make_private_with_epsilon( 
-            module=self.model, optimizer=self.optimizer, data_loader=train_dataloader,
+            module=self.llm, optimizer=self.optimizer, data_loader=train_dataloader,
             target_delta=self.delta, target_epsilon=self.epsilon, epochs=epochs,
             max_grad_norm=self.max_gradient_norm)
         
@@ -51,7 +55,6 @@ class Soft_Prompt():
                 optimizer=optimizer
             ) as memory_safe_data_loader:
                 for step, batch in enumerate(tqdm(memory_safe_data_loader)):
-                    optimizer.zero_grad()
         
                     batch = tuple(t.to(self.device) for t in batch)
                     inputs = {'input_ids':      batch[0],
@@ -59,17 +62,17 @@ class Soft_Prompt():
                             'token_type_ids': batch[2],
                             'labels':         batch[3]}
         
-                    outputs = llm_model(**inputs) # output = loss, logits, hidden_states, attentions
-                    
-                    preds = np.argmax(outputs[:2].detach().cpu().numpy(), axis=1)
+                    outputs = llm_model(**inputs) 
+                    preds = np.argmax(outputs[1].detach().cpu().numpy(), axis=1)
                     labels = inputs['labels'].detach().cpu().numpy()
                     accuracy = (preds == labels).mean()
                     accuracies.append(accuracy)
                     
                     loss = outputs[0]
-                    loss.backward()
                     losses.append(loss.item())
-        
+                    
+                    optimizer.zero_grad()
+                    loss.backward()
                     optimizer.step()
         
                     if step > 0 and step % 5000 == 0:
@@ -90,3 +93,31 @@ class Soft_Prompt():
 
     def test (self, test_dataloader):
         
+        loss_arr = []
+        accuracy_arr = []
+    
+        for batch in test_dataloader:
+            batch = tuple(t.to(self.device) for t in batch)
+    
+            with torch.no_grad():
+                inputs = {'input_ids':      batch[0],
+                          'attention_mask': batch[1],
+                          'token_type_ids': batch[2],
+                          'labels':         batch[3]}
+    
+                outputs = self.llm(**inputs)
+                loss, logits = outputs[:2]
+                
+                preds = np.argmax(logits.detach().cpu().numpy(), axis=1)
+                labels = inputs['labels'].detach().cpu().numpy()
+                
+                loss_arr.append(loss.item())
+                accuracy = (preds == labels).mean()
+                accuracy_arr.append(accuracy(preds, labels))
+        print(
+         
+          f"test loss: {np.mean(loss_arr):.3f} | "
+          f"test accuracy: {np.mean(accuracy_arr):.3f} | "
+          f"total parameter: {self.total_parameter} | "
+
+        )
