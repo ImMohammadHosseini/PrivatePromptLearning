4941619bfa2a75b5e4d3020e71df6a745d8b15e6
ImMohammadHosseini <sayedmh50@gmail.com>
2023-12-07 10:58:18 +0330

    add llm_format for handling llm in one class

---
+++ src/llm_format.py
@@ -0,0 +1,36 @@
+
+"""
+
+"""
+import torch
+from pytorch import nn
+from transformers import AutoModelForSequenceClassification
+
+
+class LLM_Format (nn.Module):
+    """
+    """
+
+    def __init__(self, model_name):
+        super().__init__()
+        self.llm = AutoModelForSequenceClassification.from_pretrained("prajjwal1/"+ model_name)
+
+    def llm_inner_embedding_output (self, xi, prompt_embedding):
+        return self.llm.bert.embeddings(xi) + prompt_embedding
+    
+    def llm_withou_inner_embedding (self, embeddings_output):
+        yi = self.llm.bert.encoder(embeddings_output)
+        yi = self.llm.bert.pooler(yi[0])
+        yi = self.llm.dropout(yi)
+        return self.llm.classifier(yi)
+    
+    def forward(self, xis, pts_list = None):
+        if pts_list == None:
+            return self.llm(xis['input_ids'])
+        
+        elif len(pts_list) == 1:
+            return self.llm_withou_inner_embedding(
+                self.llm_inner_embedding_output(xis['input_ids'], 
+                                                 torch.cat(pts_list*xis['input_ids'].size(0), 0)))[0]#.detach()
+        elif len(pts_list) > 1:
+            pass
